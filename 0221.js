835b374182e5971145751fa21f5b220a3ceca284
chestnut1044 <wbk1620@gmail.com>
2022-02-21 22:23:35 +0900

    Learn : JavaScript : DataType

---
+++ 0221.js
@@ -0,0 +1,53 @@
+// // 문자열 타입
+// var string;
+// string = '문자열';  // 작은 따옴표
+// string = "문자열";  // 큰 따옴표
+// string = `문자열`;  // 백틱(ES6)
+
+
+
+// var string = `Hello
+// world`;
+// // 출력결과
+// // Hello
+// // world
+
+
+// var first = 'Bo-kyoung'
+// var last = 'Hwang'
+
+// // ES5 : 문자열 연결
+// console.log('My name is ' + first + last + '.');
+
+
+// var first = 'Bo-kyoung'
+// var last = 'Hwang'
+
+// // ES6 : 표현식 삽입
+// console.log(`My name is ${first} ${last}.`);
+
+
+
+// var foo = true;
+// console.log(foo);
+
+// foo = false
+// console.log(foo);
+
+// foo = True
+// console.log(foo)
+
+
+
+// // 심벌값 생성
+// var key = Symbol('key');
+// console.log(typeof key); // symbol
+
+// // 객체 생성
+// var obj = {};
+
+// // 이름이 충돌할 위험이 없는 유일무이한 값인 심벌을 프로퍼티 키로 사용한다.
+// obj[key] = 'value';
+// console.log(obj[key]); // value
+
+
